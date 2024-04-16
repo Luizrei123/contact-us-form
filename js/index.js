4670aa30ed56925a4331af611c597fbaf1e0c3ff
Luiz Fernando Nascimento <luizfernando.cg@hotmail.com>
2024-04-16 12:12:35 -0300

    Criado uma função para validar Inputs e trocar o tipo de dado do campo de data
    Criado uma função para validar  os Inputs de nome e sobrenome ,e trocar o tipo de dado do campo de data atual para o tipo data quando em foco e  tipo texto quando o campo estiver  com uma data invalida ou vazia.

--- js/index.js
+++ js/index.js
@@ -2,8 +2,35 @@ const $formOne = $('.step.one');
 const $formTwo = $('.step.two');
 const $formThree = $('.step.three');
 
+const $inputNome = $('#nome');
+const $inputSobrenome = $('#sobrenome');
+const $inputDataNascimento = $('#dataNascimento');
+
 const $stepText = $('#step-text');
 const $stepDescription = $('#step-description');
+
+const minLength = 2;
+const minLengthData = 8;
+
+let nomeValido = false;
+let sobrenomeValido = false;
+let emailValido = false;
+let dataNascimentoValido = false;
+
+function validarInput(element, minLength, maxLength) {
+  const closest = $(element).closest('.input-data');
+
+  if (
+    !element.value ||
+    element.value.trim().length < minLength ||
+    element.value.trim().length > maxLength
+  ) {
+    closest.addClass('error');
+    return false;
+  }
+  closest.removeClass('error');
+  return true;
+}
 
 function init() {
   $formTwo.hide();
@@ -13,6 +40,28 @@ function init() {
   $stepDescription.text(
     'Descreva seus dados para que possamos te conhecer melhor.'
   );
+
+  $inputNome.keyup(function () {
+    nomeValido = validarInput(this, minLength);
+  });
+
+  $inputSobrenome.keyup(function () {
+    sobrenomeValido = validarInput(this, minLength);
+  });
+
+  $inputDataNascimento.keyup(function () {
+    dataNascimentoValido = validarInput(this, minLengthData);
+  });
 }
+
+$inputDataNascimento.on('focus', function () {
+  this.type = 'date';
+});
+
+$inputDataNascimento.on('blur', function () {
+  if (!this.value) {
+    this.type = 'text';
+  }
+});
 
 init();
